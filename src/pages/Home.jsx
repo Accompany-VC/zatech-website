ab30dc502d8852a7a7049bb7d16d5e6488a3b891
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2025-09-29 11:12:07 +0200

    Css styling fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,32 @@ export default function Home() {
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the Zatech website!</p>
+
+      <div
+        style={{
+          position: "relative",
+          paddingBottom: "56.25%",
+          height: 0,
+          overflow: "hidden",
+          marginTop: "2rem",
+        }}
+      >
+        <iframe
+          src="https://www.youtube.com/embed/Ze_C-Fwz_Ec"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
     </div>
   );
 }

1b555e1a96f54dc047b201574498c3cc1a9fb996
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2025-09-29 11:18:31 +0200

    Fixed in line styling

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,43 +1,18 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css"; 
+
 export default function Navbar() {
   return (
-    <nav style={{ width: "100%",
-        background: "#fff",
-        color: "black",
-        padding: "1rem 2rem",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        zIndex: 1000,
-        borderBottom: "1px solid #ddd", }}>
+    <nav className="navbar">
+      <h2 className="navbar-logo">ZA Tech</h2>
 
-      <h2 style={{ margin: 0, fontSize: "1.5rem", fontWeight: "bold" }}>ZA Tech</h2>  
-
-      <div style={{ display: "flex", gap:"2rem"}}>
-      <Link to="/" style={{ color: "black", textDecoration: "none" }}>
-        Home
-      </Link>
-      <Link to="/faq" style={{ color: "black", textDecoration: "none" }}>
-        FAQ
-      </Link>
-      <Link to="/wiki" style={{ color: "black", textDecoration: "none" }}>
-        Wiki
-      </Link>
-      <Link to="/contact" style={{ color: "black", textDecoration: "none" }}>
-        Contact
-      </Link>
-      <Link to="/sponsorship" style={{ color: "black", textDecoration: "none" }}>
-        Sponsorship
-      </Link>
-      <Link to="/joinchannel" style={{ color: "black", textDecoration: "none" }}>
-        Join
-      </Link>
-      <Link to="/wiki" style={{ color: "black", textDecoration: "none" }}>
-        Wiki
-      </Link>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/faq" className="nav-link">FAQ</Link>
+        <Link to="/wiki" className="nav-link">Wiki</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+        <Link to="/sponsorship" className="nav-link">Sponsorship</Link>
+        <Link to="/joinchannel" className="nav-link">Join</Link>
       </div>
     </nav>
   );

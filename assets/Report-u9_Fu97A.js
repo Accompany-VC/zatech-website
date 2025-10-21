228a9a40fbc0d54a259166457255b243dce9d0ca
AbdulDavids <125186956+AbdulDavids@users.noreply.github.com>
2025-10-21 21:50:24 +0000

    Deploying to gh-pages from @ Accompany-VC/zatech-website@cc9f2bbcbd4e2a1877c7068ab21b569b144ac3f8 🚀

--- assets/Report-u9_Fu97A.js
+++ assets/Report-u9_Fu97A.js
@@ -1,4 +1,4 @@
-import{b as Ol,r as ee,j as K,P as xl}from"./index-BjeEhwe8.js";const kl=()=>{};var Oi={};/**
+import{b as Ol,r as ee,j as K,P as xl}from"./index-8GC_PHtY.js";const kl=()=>{};var Oi={};/**
  * @license
  * Copyright 2017 Google LLC
  *
